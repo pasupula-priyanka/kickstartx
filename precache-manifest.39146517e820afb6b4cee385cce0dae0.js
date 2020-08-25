0046e04cde2f39caebd968e5f5960a1c8fe7b432
pasupula priyanka <pasupulapriyanka123@gmail.com>
2020-08-25 08:16:10 +0530

    Updates

---
+++ precache-manifest.39146517e820afb6b4cee385cce0dae0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b42d625d102f5b82c1b1fddc2ecdf46",
+    "url": "/kickstartx/index.html"
+  },
+  {
+    "revision": "8cdd2545aff5e3ad91da",
+    "url": "/kickstartx/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "eac47017c400544237d5",
+    "url": "/kickstartx/static/css/main.9ee9cc8c.chunk.css"
+  },
+  {
+    "revision": "8cdd2545aff5e3ad91da",
+    "url": "/kickstartx/static/js/2.98ee01a6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/kickstartx/static/js/2.98ee01a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eac47017c400544237d5",
+    "url": "/kickstartx/static/js/main.2bad5c85.chunk.js"
+  },
+  {
+    "revision": "bf2950471c3f95cc1f1a",
+    "url": "/kickstartx/static/js/runtime-main.dbaf6fa1.js"
+  }
+]);
